dbfa6f37408af34bfab28ddb215e4b4ee545d15c
Emmanuel Mayssat <emmanuel.mayssat@aya.yale.edu>
2021-11-16 22:52:02 -0800

    more

--- pcl.mk
+++ pcl.mk
@@ -4,6 +4,7 @@ _PCL_MK_VERSION= 0.99.6
 PCL_AWSPROFILE_NAME?= adfs
 # PCL_IAMROLE_ARN?= arn:aws:iam::123456789012:role/tfe-module-pave
 # PCL_IAMROLE_NAME?= tfe-module-pave
+PCL_UI_LABEL?= [pcl] #
 PCL_USER_DOMAIN_NAME?= NAEAST
 PCL_USER_SANDBOX_FLAG?= false
 # PCL_USER_SID?= A123456
@@ -13,38 +14,35 @@ PCL_AWSACCOUNT_ID?= $(AWS_ACCOUNT_ID)
 PCL_AWSPROFILE_NAME?= $(AWS_PROFILE_NAME)
 PCL_IAMROLE_ARN?= $(if $(PCL_IAMROLE_NAME),arn:aws:iam::$(PCL_AWSACCOUNT_ID):role/$(PCL_IAMROLE_NAME))
 
-# Options parameters
+# Options
 __PCL_DOMAIN= $(if $(PCL_USER_DOMAIN_NAME),--domain $(PCL_USER_DOMAIN_NAME))
 __PCL_PROFILE_NAME= $(if $(PCL_AWSPROFILE_NAME),--profile-name $(PCL_AWSPROFILE_NAME))
 __PCL_ROLE_ARN= $(if $(PCL_IAMROLE_ARN),--role-arn $(PCL_IAMROLE_ARN))
 __PCL_SANDBOX_USER= $(if $(filter true, $(PCL_USER_SANDBOX_FLAG)),--sandbox-user)
 __PCL_SID= $(if $(PCL_USER_SID),--sid $(PCL_USER_SID))
 
-# Pipe parameters
+# Customizations
 _PCL_REFRESH_PROFILE_|?= # while true; do
 |_PCL_REFRESH_PROFILE?= # ; sleep 60; done
 
-# UI parameters
-PCL_UI_LABEL?= [pcl] #
+# Macros
 
-#--- Utilities
+# Utilities
 # _PCL_ENVIRONMENT+= AWS_CA_BUNDLE=''
 PCL_BIN?= pcl
 PCL?= $(strip $(_PCL_ENVIRONMENT) $(PCL_ENVIRONMENT) $(PCL_BIN) $(_PCL_OPTIONS) $(PCL_OPTIONS))
-
-#--- MACROS
 
 #----------------------------------------------------------------------
 # USAGE
 #
 
-_view_framework_macros :: _pcl_view_framework_macros
-_pcl_view_framework_macros ::
+_list_macros :: _pcl_list_macros
+_pcl_list_macros ::
 	@echo 'PCL ($(_PCL_MK_VERSION)) macros:'
 	@echo
 
-_view_framework_parameters :: _pcl_view_framework_parameters
-_pcl_view_framework_parameters ::
+_list_parameters :: _pcl_list_parameters
+_pcl_list_parameters ::
 	@echo 'PCL ($(_PCL_MK_VERSION)) parameters:'
 	@echo '    PCL_AWSACCOUNT_ID=$(PCL_AWSACCOUNT_ID)'
 	@echo '    PCL_AWSPROFILE_NAME=$(PCL_AWSPROFILE_NAME)'
@@ -56,8 +54,8 @@ _pcl_view_framework_parameters ::
 	@echo '    PCL=$(PCL)'
 	@echo
 
-_view_framework_targets :: _pcl_view_framework_targets
-_pcl_view_framework_targets ::
+_list_targets :: _pcl_list_targets
+_pcl_list_targets ::
 	@echo 'PCL ($(_PCL_MK_VERSION)) targets:'
 	@echo '    _pcl_refresh_awsprofile              - Refresh credentials in AWS-profile'
 	@echo 
@@ -69,6 +67,7 @@ _pcl_view_framework_targets ::
 #----------------------------------------------------------------------
 # PUBLIC TARGETS
 #
+
 _pcl_refresh_awsprofile:
 	@$(INFO) '$(PCL_UI_LABEL)Refreshing credentials stored in AWS-profile "$(PCL_AWSPROFILE_NAME)" ...'; $(NORMAL)
 	$(_PCL_REFRESH_AWSPROFILE_|)$(PCL) aws login $(__PCL_DOMAIN) $(__PCL_PROFILE_NAME) $(__PCL_ROLE_ARN) $(__PCL_SANDBOX_USER) $(__PCL_SID) $(|_PCL_REFRESH_AWSPROFILE)
