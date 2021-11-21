32f397a434e11f69f21397b12947b074695697e2
Emmanuel Mayssat <emmanuel.mayssat@aya.yale.edu>
2021-11-21 00:22:13 -0800

    more

--- skaffold_config.mk
+++ skaffold_config.mk
@@ -29,10 +29,11 @@ _SFD_LIST_CONFIGS_|?= $(if $(SFD_CONFIGS_APPLICATIONS_DIRPATH),cd $(SFD_CONFIGS_
 _SFD_LIST_CONFIGS_SET_|?= $(_SFD_LIST_CONFIGS_|)
 |_SFD_LIST_CONFIGS?=
 |_SFD_LIST_CONFIGS_SET?=
+_SFD_READ_CONFIG_|?= $(_SFD_CREATE_CONFIG_|)
 _SFD_SHOW_CONFIG_|?= $(if $(SFD_CONFIG_APPLICATION_DIRPATH),cd $(SFD_CONFIG_APPLICATION_DIRPATH) && )#
 _SFD_SHOW_CONFIG_CONTENT_|?= $(_SFD_SHOW_CONFIG_|)
 _SFD_SHOW_CONFIG_DESCRIPTION_|?= $(_SFD_SHOW_CONFIG_|)
-_SFD_WRITE_CONFIG_|?= $(_SFD_CREATE_CONFIG_|)
+_SFD_WRITE_CONFIG_|?= $(_SFD_READ_CONFIG_|)
 
 # Macros
 
@@ -64,6 +65,7 @@ _sfd_list_targets ::
 	@echo '    _sfd_delete_config             - Delete an existing config'
 	@echo '    _sfd_list_configs              - List all configs'
 	@echo '    _sfd_list_configs_set          - List set of configs'
+	@echo '    _sfd_read_config               - Read a config'
 	@echo '    _sfd_show_config               - Show everything related to a config'
 	@echo '    _sfd_show_config_content       - Show the content of a config'
 	@echo '    _sfd_show_config_description   - Show the description of a config'
@@ -114,6 +116,10 @@ _sfd_list_configs:
 _sfd_list_configs_set:
 	@$(INFO) '$(SFD_UI_LABEL)Listing configs-set "$(SFD_CONFIGS_SET_NAME)" ...'; $(NORMAL)
 	# $(_SFD_LIST_CONFIGS_SET_|)
+
+_sfd_read_config:
+	@$(INFO) '$(SFD_UI_LABEL)Reading config "$(SFD_CONFIG_NAME)" ...'; $(NORMAL)
+	$(_SFD_READ_CONFIG_|)$(READER) $(SFD_CONFIG_FILEPATH)
 
 _SFD_SHOW_CONFIG_TARGETS?= _sfd_show_config_content _sfd_show_config_description
 _sfd_show_config: $(_SFD_SHOW_CONFIG_TARGETS)
